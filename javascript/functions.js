62d0a7b64d21648b0b50162bee3b9067867a81a6
LindaBaars <lindabaars@gmail.com>
2017-09-30 02:38:42 +0200

    Add files via upload

--- javascript/functions.js
+++ javascript/functions.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 	//alert("documebnt " + document.URL);
 	if(navigator.userAgent.indexOf("Mac") != -1) {
-		//alert(window.location);
+		alert(localStorage.getItem("visited"));
 		if(!localStorage.getItem("visited")){
 			localStorage.setItem("visited",true);
 			window.location.href = "https://lindabaars.github.io/lindabaarsnl/";
